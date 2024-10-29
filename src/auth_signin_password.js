b789b7aca7afd8d24996c3b5d7b696dd26244adf
msuharit <msuharitdumrong@gmail.com>
2024-10-28 18:16:49 -0700

    stuff

---
+++ src/auth_signin_password.js
@@ -0,0 +1,13 @@
+import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-auth.js";
+
+const auth = getAuth();
+signInWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+  });
