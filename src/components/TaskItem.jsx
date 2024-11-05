04881ae6933b8d84fc03da3383f0162fb15b6955
LeJin27 <jiqle@ucsc.edu>
2024-11-05 11:50:47 -0800

    added double click to edit

--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -54,43 +54,51 @@ const TaskItem = ({
         };
     }, [isEditingAlarm, index]);
 
-    return ( 
-            <div className = "flex-1">
+
+
+
+
+    const handleDoubleClick = () => {
+        setEditingIndex(index);
+        setEditTaskText(task.task_desc); // Set the current task description for editing
+    };
+
+    return (
+        <div className="flex-1">
 
             <div
-                className={`text-left task ${task.task_is_completed ? 'completed hover:rounded-xl hover:bg-[#AFDD66]  transition-all duration-300' 
+                className={`text-left task ${task.task_is_completed ? 'completed hover:rounded-xl hover:bg-[#AFDD66]  transition-all duration-300'
                     : 'uncompleted hover:rounded-xl hover:bg-[#3AA7FA] transition-all duration-300'}`}
                 onContextMenu={(e) => onRightClick(e, index)}
+                onDoubleClick={handleDoubleClick}
             >
                 {isEditing ? (
-                    <div class = "text-black">
-                    <input
-                        type="text"
-                        value={editTaskText}
-                        onChange={(e) => setEditTaskText(e.target.value)}
-                        onBlur={() => onEditTask(index)}
-                        onKeyPress={(e) => e.key === 'Enter' && onEditTask(index)}
-                    />
+                    <div class="text-black ">
+                        <input class=""
+                            type="text"
+                            value={editTaskText}
+                            onChange={(e) => setEditTaskText(e.target.value)}
+                            onBlur={() => onEditTask(index)}
+                            onKeyPress={(e) => e.key === 'Enter' && onEditTask(index)}
+                        />
                     </div>
                 ) : (
-                            <div>
-                            <div className="task-text">{task.task_desc} </div>
-                            <div className="absolute bottom-2 right-2 text-xs"> {task.task_created_time_stamp}</div>     
-                            </div>
+                    <div>
+                        <div className="task-text">{task.task_desc} </div>
+                        <div className="absolute bottom-2 right-2 text-xs"> {task.task_created_time_stamp}</div>
+                    </div>
                 )}
-                
                 {task.task_alarm_time && (
                     <span className="alarm-time"> ALARM: {new Date(task.task_alarm_time).toLocaleString()}</span>
                 )}
-
-                <button 
-                    className="alarm-edit-button" 
-                    onClick={() => 
+                <button
+                    className="alarm-edit-button"
+                    onClick={() =>
                         setIsEditingAlarm(!isEditingAlarm)
                     }
                     title="Set an alarm"
                 >
-                <FaBell style={{ color: isEditingAlarm ? 'blue' : 'white' }} />
+                    <FaBell style={{ color: isEditingAlarm ? 'blue' : 'white' }} />
                 </button>
                 {isEditingAlarm && (
                     <input
@@ -100,7 +108,7 @@ const TaskItem = ({
                 )}
 
             </div>
-            </div>
+        </div>
 
     );
 };
