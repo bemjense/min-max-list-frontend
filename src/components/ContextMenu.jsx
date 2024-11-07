497a95e52f502aa95a9044893736690087e907fc
LeJin27 <jiqle@ucsc.edu>
2024-11-07 11:12:18 -0800

    made context menu match tehme

--- src/components/ContextMenu.jsx
+++ src/components/ContextMenu.jsx
@@ -1,14 +1,27 @@
 import React from 'react';
 import './ContextMenu.css'; // Import the CSS file
 
+import { FaRegTrashCan } from "react-icons/fa6";
+import { IoCheckmarkDoneOutline } from "react-icons/io5";
+import { FaEdit} from 'react-icons/fa';
+import { HiMiniTrash } from "react-icons/hi2";
+import { RiMarkPenFill } from "react-icons/ri";
+
 const ContextMenu = ({ top, left, onAction, isCompleted }) => (
     <div className="context-menu fade-in" style={{ position: 'absolute', top, left }}>
         <ul>
-            <li onClick={() => onAction('edit')}>✏️ Edit</li>
+            <li onClick={() => onAction('edit')}>️
+                <FaEdit className='mr-2'/>
+                
+                Edit</li>
             <li onClick={() => onAction('toggle')}>
-                ✅ {isCompleted ? 'Undo Complete' : 'Mark as Complete'}
+                <RiMarkPenFill className = 'mr-2'/>
+                {isCompleted ? 'Unmark' : 'Mark'}
             </li>
-            <li onClick={() => onAction('delete')}>🗑️ Delete</li>
+
+            <li onClick={() => onAction('delete')}>
+                <HiMiniTrash className = 'mr-2'/> Delete
+            </li>
         </ul>
     </div>
 );
