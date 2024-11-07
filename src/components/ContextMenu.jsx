d96e7ead0c74197d06e3bbfd97d95ed0c3db5a34
LeJin27 <jiqle@ucsc.edu>
2024-11-07 15:29:36 -0800

    updated context menu to have alarm now

--- src/components/ContextMenu.jsx
+++ src/components/ContextMenu.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './ContextMenu.css'; // Import the CSS file
 
+import { FaBell} from 'react-icons/fa';
 import { FaRegTrashCan } from "react-icons/fa6";
 import { IoCheckmarkDoneOutline } from "react-icons/io5";
 import { FaEdit} from 'react-icons/fa';
@@ -24,7 +25,7 @@ const ContextMenu = ({ top, left, onAction, isCompleted }) => (
             </li>
 
             <li onClick={() => onAction('alarm')}>
-                <HiMiniTrash className = 'mr-2'/> Alarm
+                <FaBell className = 'mr-2'/> Alarm
             </li>
         </ul>
     </div>
