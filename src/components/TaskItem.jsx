a17bc8997dd1e3e33e15095c634e63b885bf4d1b
LeJin27 <jiqle@ucsc.edu>
2024-11-07 09:48:16 -0800

    added edit icon

--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -1,6 +1,6 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState, useRef, useEffect, useCallback} from 'react';
 import './TaskItem.css'; 
-import { FaBell } from 'react-icons/fa';
+import { FaBell, FaEdit} from 'react-icons/fa';
 import AirDatepicker from 'air-datepicker';
 import 'air-datepicker/air-datepicker.css';
 import localeEn from 'air-datepicker/locale/en';
@@ -41,6 +41,8 @@ const TaskItem = ({
             setIsEditing(false); // Ensure editing state is reset when not matching
         }
     }, [editID, task]);
+
+
 
 
 
@@ -109,6 +111,14 @@ const TaskItem = ({
         }
     };
 
+
+    const handleEditButton = useCallback(() => {
+        setEditID(task.task_id)
+        const newDesc = task.task_desc;
+        setEditText(newDesc);  // Update local state first
+        setIsEditing(true)
+    }, [editID, task]);
+
     return (
         <div className="flex-1 text-[0.8rem] font-semibold">
 
@@ -120,11 +130,14 @@ const TaskItem = ({
 
 
 
+
+
+
                 {/*if state of task is currently editing then return userinput prompt else return normal render */}
                 {isEditing ? (
                     <div class={`${task.task_is_completed ? 'text-black '
-                    : 'text-white'}`}>
-                        <input 
+                        : 'text-white'}`}>
+                        <input
                             className="task-input bg-transparent p-2 outline-none focus:outline-none w-full ml-5"
                             type="text"
                             value={editText}
@@ -137,28 +150,41 @@ const TaskItem = ({
                     <div>
 
                         {/*Normal Render description and text*/}
+                        <div className = "flex">
                         <div className="task-text ml-4">{task.task_desc} </div>
+
+                        <button onClick={handleEditButton} className='ml-2'>
+                            <FaEdit style={{ color: task.task_is_completed ? 'black' : 'white' }} />
+                        </button>
+                        </div>
                         <div className="absolute bottom-2 right-2 text-xs mr-6"> {helperGetTaskDate(task)}</div>
                     </div>
                 )}
-                <button
-                    className="alarm-edit-button ml-4"
-                    onClick={() =>
-                        setIsEditingAlarm(!isEditingAlarm)
-                    }
-                    title="Set an alarm"
-                >
-                    <FaBell style={{ color: isEditingAlarm ? 'blue' : 'white' }} />
-                </button>
-                {isEditingAlarm && (
-                    <input
-                        ref={dateTimePickerRef}
-                        className="hidden-datepicker"
-                    />
-                )}
-                {task.task_alarm_time && (
-                    <span className="alarm-time ml-1"> {new Date(task.task_alarm_time).toLocaleString()}</span>
-                )}
+
+
+
+                <div className="flex">
+
+                    <button
+                        className="alarm-edit-button ml-4"
+                        onClick={() =>
+                            setIsEditingAlarm(!isEditingAlarm)
+                        }
+                        title="Set an alarm"
+                    >
+                        <FaBell style={{ color: task.task_is_completed ? 'black' : 'white' }} />
+                    </button>
+
+                    {isEditingAlarm && (
+                        <input
+                            ref={dateTimePickerRef}
+                            className="hidden-datepicker"
+                        />
+                    )}
+                    {task.task_alarm_time && (
+                        <span className="alarm-time ml-1"> {new Date(task.task_alarm_time).toLocaleString()}</span>
+                    )}
+                </div>
 
             </div>
         </div>
