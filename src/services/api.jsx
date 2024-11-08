fe53d1e5bd58bbadc665283e2a66a78e569a6c65
LeJin27 <jiqle@ucsc.edu>
2024-11-08 09:46:45 -0800

    added reading with status funcitonaly for multiple todolsits

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -31,29 +31,48 @@ export const readTaskAtId = async (task_id) => {
     }
 };
 
-export const readUncompletedTasks = async (uid) => {
+export const readUncompletedTasks = async (uid, taskList = null) => {
     try {
-        const response = await axios.get('http://localhost:8000/tasks/?task_is_completed=false', {
-            params: { task_uid: uid } 
+        // Create an object for query parameters
+        const task_params = { task_uid: uid, task_is_completed: false };
 
-        });
+        // Add task_list to params if provided
+        if (taskList !== null) {
+            task_params.task_list = taskList;
+        }
+
+        // Send the GET request with the query parameters
+        const response = await axios.get('http://localhost:8000/tasks/', { params: task_params });
+
+        // Sort the tasks by task_id
         return response.data.sort((a, b) => a.task_id - b.task_id);
     } catch (error) {
         console.error('Error fetching tasks:', error);
         return [];
     }
 };
-export const readCompletedTasks = async (uid) => {
+
+export const readCompletedTasks = async (uid, taskList = null) => {
     try {
-        const response = await axios.get('http://localhost:8000/tasks/?task_is_completed=True', {
-            params: { task_uid: uid } 
-        });
+        // Create an object for query parameters
+        const task_params = { task_uid: uid, task_is_completed: true };
+
+        // Add task_list to params if provided
+        if (taskList !== null) {
+            task_params.task_list = taskList;
+        }
+
+        // Send the GET request with the query parameters
+        const response = await axios.get('http://localhost:8000/tasks/', { params: task_params });
+
+        // Sort the tasks by task_id
         return response.data.sort((a, b) => a.task_id - b.task_id);
     } catch (error) {
         console.error('Error fetching tasks:', error);
         return [];
     }
 };
+
 
 export const createTask = async (task_uid, task_list, task_desc,task_alarm_time) => {
     try {
