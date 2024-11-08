3ccdf321709deb923c18d9c1a97f2f4937e2ea31
LeJin27 <jiqle@ucsc.edu>
2024-11-07 17:16:19 -0800

    made contextmenu recntagle

--- src/components/ContextMenu.jsx
+++ src/components/ContextMenu.jsx
@@ -10,16 +10,21 @@ import { RiMarkPenFill } from "react-icons/ri";
 
 const ContextMenu = ({ top, left, onAction, isCompleted }) => (
     <div className="context-menu fade-in" style={{ position: 'absolute', top, left }}>
-        <ul>
-            <li onClick={() => onAction('edit')}>️
-                <FaEdit className='mr-2'/>
-                
-                Edit</li>
-            <li onClick={() => onAction('toggle')}>
-                <RiMarkPenFill className = 'mr-2'/>
-                {isCompleted ? 'Unmark' : 'Mark'}
-            </li>
+        <ul className="flex-row flex">
 
+
+            <div className='flex-col flex'>
+                <li onClick={() => onAction('edit')}>️
+                    <FaEdit className='mr-2'/>
+
+                    Edit</li>
+                <li onClick={() => onAction('toggle')}>
+                    <RiMarkPenFill className = 'mr-2'/>
+                    {isCompleted ? 'Unmark' : 'Mark'}
+                </li>
+            </div>
+
+            <div className='flex-col flex'>
             <li onClick={() => onAction('delete')}>
                 <HiMiniTrash className = 'mr-2'/> Delete
             </li>
@@ -27,6 +32,8 @@ const ContextMenu = ({ top, left, onAction, isCompleted }) => (
             <li onClick={() => onAction('alarm')}>
                 <FaBell className = 'mr-2'/> Alarm
             </li>
+            </div>
+
         </ul>
     </div>
 );
