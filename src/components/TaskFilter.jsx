4bab19772c65d0ac6a8e96bc68c00499d7526a9c
LeJin27 <jiqle@ucsc.edu>
2024-11-14 13:05:47 -0800

    modified taskfilter

--- src/components/TaskFilter.jsx
+++ src/components/TaskFilter.jsx
@@ -1,4 +1,5 @@
-
+import { MdRemoveCircle } from "react-icons/md";
+import { FaXmark  } from "react-icons/fa6";
 const TaskFilter = (
     {filterTaskCreatedTimeStamp, setFilterTaskCreatedTimeStamp}) => {
 
@@ -11,18 +12,17 @@ const TaskFilter = (
 
 
     
+        <button
+            className="text-white text-[0.8rem] bg-transparent hover:text-gray-300 "
+            onClick={() => setFilterTaskCreatedTimeStamp(null)}
+            aria-label="Clear filter"
+        >
 
-    <div className=' flex max-h-8 rounded-full bg-[#3aa7fa] mt-[0.2rem] p-[0.4rem] gap-1 motion-duration-500 motion-preset-blur-left'>
-            <button 
-                className="text-white text-[0.8rem] bg-transparent hover:text-gray-300 "
-                onClick={() => setFilterTaskCreatedTimeStamp(null)}
-                aria-label="Clear filter"
-            >
-                ✕
-            </button>
-        <div class=" text-white text-[0.8rem] text-left ">{filterTaskCreatedTimeStamp}</div>
-
-    </div>
+            <div className=' flex items-center  max-h-8 rounded-full bg-[#3aa7fa] mt-[0.2rem] p-[0.4rem] gap-1 motion-duration-500 motion-preset-blur-left'>
+                <FaXmark  className = "mt-[1px]"/>
+                <div class=" text-white text-[0.8rem] text-left ">{filterTaskCreatedTimeStamp}</div>
+            </div>
+        </button>
     )
 };
 
