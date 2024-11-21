c1cf848290d347a5a727e7c30a7fee7429cb63f8
LeJin27 <jiqle@ucsc.edu>
2024-11-21 00:52:46 -0800

    fixed checkmark task not working for completed tasks

--- src/components/TaskGrouping.jsx
+++ src/components/TaskGrouping.jsx
@@ -94,6 +94,7 @@ const TaskGrouping = ({
               setEditDueDateID={setEditDueDateID}
               handleDeleteAlarm={handleDeleteAlarm}
               handleDeleteDueDate={handleDeleteDueDate}
+              handleToggleStatus={handleToggleStatus}
             />
           ))}
         </div>
